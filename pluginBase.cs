363a8cafefe0d9aaab7f8b4a5ab030667b040573
ITLDG <wanglong126@139.com>
2023-09-06 15:03:20 +0800

    fix: 迎合主程序升级,调整代码

--- pluginBase.cs
+++ pluginBase.cs
@@ -2,13 +2,24 @@
 
 namespace SerialPortForward
 {
-    public class Base:IPlugin
+    public class Base : IPlugin
     {
-        public string Name { get; set; } = "原始数据";
 
+        public string Name { get; } = "原始数据";
+        public bool HasOption { get => false; }
+        public void Checked(PluginCommon plugin)
+        {
+        }
+        public void UnCheck()
+        {
+        }
         public byte[] GetBytes(bool IsCom1, string ComName, ref byte[] Bytes)
         {
             return null;
+        }
+
+        public void Option()
+        {
         }
     }
 }
--- pluginOriginalRep.cs
+++ pluginOriginalRep.cs
@@ -4,11 +4,23 @@ namespace SerialPortForward
 {
     public class OriginalRep : IPlugin
     {
-        public string Name { get; set; } = "原文答复";
+        public string Name => "原文答复";
+
+        public bool HasOption => false;
 
         public byte[] GetBytes(bool _, string ComName, ref byte[] Bytes)
         {
             return Bytes;
+        }
+        public void Checked(PluginCommon plugin)
+        {
+        }
+        public void UnCheck()
+        {
+        }
+        public void Option()
+        {
+
         }
     }
 }
